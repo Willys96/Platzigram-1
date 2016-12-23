5e36901d7df06db97e7598824808fe04270fb9fd
Kristian Lopez <kristian.lom@gmail.com>
2016-12-23 11:00:58 -0600

    Creación de la vista del crear cuenta, junto con el toolbar

--- app/src/main/java/co/kr1lo/platzigram/views/CreateAccountActivity.java
+++ app/src/main/java/co/kr1lo/platzigram/views/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package co.kr1lo.platzigram.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import co.kr1lo.platzigram.R;
 
@@ -11,5 +12,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolBar(getResources().getString(R.string.toolbar_tittle_createaccount),false);
+    }
+
+    public void showToolBar(String tittle, boolean upButton) {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
