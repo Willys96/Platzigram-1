4ebd3f0556df6ea22b366f7f0552ac9c20ee2197
Kristian Lopez <kristian.lom@gmail.com>
2016-12-26 12:53:40 -0600

    Mostrando el home con las imaganes utilizando el RecyclerView

--- app/src/main/java/co/kr1lo/platzigram/views/ContainerActivity.java
+++ app/src/main/java/co/kr1lo/platzigram/views/ContainerActivity.java
@@ -1,13 +1,11 @@
 package co.kr1lo.platzigram.views;
 
+import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
 
 import co.kr1lo.platzigram.R;
@@ -31,15 +29,21 @@ public class ContainerActivity extends AppCompatActivity {
                 switch (tabId) {
                     case R.id.home:
                         HomeFragment homeFragment = new HomeFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
                         break;
                     case R.id.profile:
                         ProfileFragment profileFragment = new ProfileFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
                         break;
                     case R.id.search:
                         SearchFragment searchFragment = new SearchFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
                         break;
                 }
             }
--- app/src/main/java/co/kr1lo/platzigram/views/fragment/HomeFragment.java
+++ app/src/main/java/co/kr1lo/platzigram/views/fragment/HomeFragment.java
@@ -3,11 +3,19 @@ package co.kr1lo.platzigram.views.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import co.kr1lo.platzigram.R;
+import co.kr1lo.platzigram.adapter.PictureAdapterRecyclerView;
+import co.kr1lo.platzigram.model.Picture;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,40 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        showToolBar(getResources().getString(R.string.tab_home),false,view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture,getActivity());
+
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
     }
 
+    public ArrayList<Picture> buildPictures(){
+
+        ArrayList<Picture> pictures = new ArrayList<>();
+
+        pictures.add(new Picture("http://s.hswstatic.com/gif/mt-everest-tourism-171676392.jpg","Kristian Lopez","4 dias", "3"));
+        pictures.add(new Picture("http://img1.meristation.com/files/imagenes/general/tonyhawkairtime.jpg","David Lopez","5 dias", "13"));
+        pictures.add(new Picture("http://www.cancunyatesenrenta.com/lib/images/cancun-yacht-rental-4.jpg","Elizabeth Lopez","1 dias", "7"));
+
+        return pictures;
+    }
+
+    public void showToolBar(String tittle, boolean upButton, View view) {
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 }
