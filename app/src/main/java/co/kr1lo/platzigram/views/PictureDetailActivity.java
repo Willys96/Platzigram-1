bd1443b960c24be9993b18c9689546707d9709b8
Kristian Lopez <kristian.lom@gmail.com>
2016-12-26 13:57:37 -0600

    Creación del Activity del Detalle de la imagen

---
+++ app/src/main/java/co/kr1lo/platzigram/views/PictureDetailActivity.java
@@ -0,0 +1,15 @@
+package co.kr1lo.platzigram.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import co.kr1lo.platzigram.R;
+
+public class PictureDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+    }
+}
