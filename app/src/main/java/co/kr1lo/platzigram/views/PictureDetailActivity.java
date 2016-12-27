78889adbb7d7dffef7c3b85dab6a48c87ec8ebca
Kristian Lopez <kristian.lom@gmail.com>
2016-12-27 11:35:48 -0600

    Uso de transacciones al seleccionar una imagen

--- app/src/main/java/co/kr1lo/platzigram/views/PictureDetailActivity.java
+++ app/src/main/java/co/kr1lo/platzigram/views/PictureDetailActivity.java
@@ -1,10 +1,13 @@
 package co.kr1lo.platzigram.views;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
 
 import co.kr1lo.platzigram.R;
 
@@ -15,6 +18,10 @@ public class PictureDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_detail);
         showToolBar("",true);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            //getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setEnterTransition(new Fade());
+        }
     }
 
     public void showToolBar(String tittle, boolean upButton) {
